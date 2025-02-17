d501199667dc544273200711926709209fb7ea81
Saracchandra Cheedella <sarac2302@gmail.com>
2025-02-17 03:40:13 -0600

    Updated files

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+export const LoadingScreen = ({ onComplete }) => {
+  const [text, setText] = useState("");
+  const fullText = "<If we want users to like our software, we should design it to behave like a likable person : Alan Cooper />";
+
+  useEffect(() => {
+    let index = 0;
+    const interval = setInterval(() => {
+      setText(fullText.substring(0, index));
+      index++;
+
+      if (index > fullText.length) {
+        clearInterval(interval);
+
+        setTimeout(() => {
+          onComplete();
+        }, 1000);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [onComplete]);
+
+  return (
+    <div
+      className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center text-center"
+      style={{
+        background: "url('/Saracchandra-Website/stars.jpg') no-repeat center center fixed",  
+        backgroundSize: 'cover',  
+      }}
+    >
+   
+      <div className="mb-4 text-4xl font-mono font-bold">
+        {text} <span className="animate-blink ml-1"> | </span>
+      </div>
+
+     
+      <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden mb-6">
+        <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+      </div>
+    </div>
+  );
+};
